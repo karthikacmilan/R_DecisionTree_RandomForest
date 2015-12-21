21e5ea41926e31040bc139961fe98cca28c36424
Karthik <ksockali@asu.edu>
2015-12-20 19:16:47 -0700

    decision tree commit

--- decisiontree.r
+++ decisiontree.r
@@ -5,12 +5,13 @@ attributes(HEC)
 #training set and test set 
 #Decision tree
 set.seed(1234)
+#splitting 80%-20% 
 samplespace = sample(2,nrow(HEC),replace=TRUE,prob = c(0.8,0.2))
 trainingdata = HEC[samplespace==1,]
 testdata = HEC[samplespace==2,]
 #train a decision tree
 library(rpart)
-space = V3~V1+V2+V4+V5
+space = V5~V1+V2+V4+V3
 space_rpart = rpart(space,data = trainingdata , control = rpart.control(minsplit = 5))
 attributes(space_rpart)
 print(space_rpart)
@@ -24,3 +25,20 @@ print(sp_prune)
 #plotting the decision tree
 plot(sp_prune)
 text(sp_prune,use.n = T)
+#saving initial scater plot to pdf
+library(ggplot2)
+pdf("plot.pdf")
+qplot(V1,V2,data=HEC,facets=V3 ~.)
+graphics.off()
+#saving decision tree to pdf
+pdf("dt.pdf")
+plot(sp_prune)
+text(sp_prune,use.n = T)
+graphics.off()
+#prediction result
+pre = predict(sp_prune, newdata = testdata)
+xlim = range(HEC$V5)
+plot(pre ~ V5, data = testdata , xlab= "observed", + ylab = "prediction", ylim = xlim, xlim = xlim)
+abline(a=0,b=1)
+
+
